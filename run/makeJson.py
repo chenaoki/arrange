3afa44dd0522f1db77a8e3583f2f47e4c942da69
Naoki Tomii <chenaoki@gmail.com>
2015-08-20 14:41:14 +0900

    実行スクリプト・query文によるDBからのsession選択を可能に

--- run/makeJson.py
+++ run/makeJson.py
@@ -7,15 +7,15 @@ from datetime import date, datetime, timedelta
 if __name__ == '__main__':
 
     if not len( sys.argv ) is 5:
-        print 'usage : makeJson [source directory]  [save directory]  [analysis engine]  [camera name]'
+        print 'usage : makeJson [source directory]  [save directory]  [analysis engine] [query where]'
         print 'ex:'
-        print 'makeJson /Volumes/Recordings/ExperimentData ./ optical sa4'
+        print 'makeJson \n\t/Volumes/Recordings/ExperimentData \n\t/Volumes/Recordings/AnalysisResult \n\toptical \n\t"WHERE sample_id = 1"'
         exit()
 
-    sourceDirPath = sys.argv[1] # ex. "/Volumes/Recordings/AnalysisResult"
+    sourceDirPath = sys.argv[1]
     saveDirPath = sys.argv[2]
     engine =  sys.argv[3]
-    camera_name = sys.argv[4]
+    where =  sys.argv[4]
 
     # Connection to MySQL Server
     con = mysql.connector.connect(
@@ -24,70 +24,45 @@ if __name__ == '__main__':
         user='tomii',
         passwd='feedback',
         buffered=True)
-    cur = con.cursor()
+    cur = con.cursor(dictionary=True)
 
-    cur.execute("select id from cameras where name = \"%s\"" % camera_name )
-    camera_id = cur.fetchone()[0]
-    print
-    cur.execute( 'select id from samples where camera_id = \"%s\"'  % camera_id)
-    sample_list = [ res[0] for res in cur.fetchall() ]
+    cur.execute( 'select * from sessions %s'  % where)
+    session_list = cur.fetchall()
 
-    print camera_id
-    print sample_list
+    for ses in session_list:
+        cur.execute( 'select * from samples where id = %s' % ses["sample_id"])
+        sample_info = cur.fetchone()
+        cur.execute( 'select * from cameras where id = %s' % sample_info["camera_id"])
+        camera_info = cur.fetchone()
 
-    for sample_id in sample_list:
+        #print ses
+        #print sample_info
+        #print camera_info
 
-        cur.execute( 'select name, mask from samples where id = \"%s\"' % sample_id)
-        res = cur.fetchone()
-        sample_name = res[0]
-        flg_mask = int(res[1])
-        cur.execute( 'select id, name, img_size from sessions where sample_id = \"%s\" and spiral = 1' % sample_id )
-        session_list = [ {"id" : res[0], "name" : res[1], "img_size" : res[2] }  for res in cur.fetchall()] # (id, name) tupple
+        # file name prefix setting
+        if camera_info["name"] == "max":
+            filePrefix =ses["name"].lower()
+        if camera_info["name"] == "sa4":
+            filePrefix = ses["name"]
+        if camera_info["name"] == "dalsa":
+            filePrefix = "f_"
+        filePrefix = filePrefix.replace('@', '')
 
-        print sample_name
+        # edit template
+        f =  open("template/%s_%s.template" % ( engine, camera_info["name"] ), "r" )
+        fileStr = f.read()
+        f.close()
 
-        for ses in session_list:
+        fileStr = fileStr.replace('SOURCE_DIR', sourceDirPath)
+        fileStr = fileStr.replace('SAVE_DIR', saveDirPath)
+        fileStr = fileStr.replace('SAMPLE_NAME', sample_info["name"])
+        fileStr = fileStr.replace('SESSION_NAME', ses["name"])
+        fileStr = fileStr.replace('FILE_PREFIX', filePrefix)
+        fileStr = fileStr.replace('IMG_SIZE', str(ses["img_size"]))
+        if sample_info["mask"] > 0:
+            fileStr = fileStr.replace('MASK_IMG_NAME', sample_info["name"])
+        else:
+            fileStr = fileStr.replace('MASK_IMG_NAME', "white")
 
-            print ses["name"]
-
-            if camera_name == "max":
-                filePrefix =ses["name"].lower()
-            if camera_name == "sa4":
-                filePrefix = ses["name"]
-            if camera_name == "dalsa":
-                filePrefix = "f_"
-            filePrefix = filePrefix.replace('@', '')
-
-            img_size = ses["img_size"]
-
-            cmd = "sed -e \'s/SOURCE_DIR/%s/g\' template/%s_%s.template >  temp1.json" % (  sourceDirPath.replace('/', '\/'), engine, camera_name  )
-            print cmd
-            os.system(cmd)
-            cmd = "sed -e \'s/SAVE_DIR/%s/g\' temp1.json >  temp2.json" % saveDirPath.replace('/', '\/')
-            print cmd
-            os.system(cmd)
-            cmd = "sed -e \'s/SAMPLE_NAME/%s/g\' temp2.json >  temp1.json" % sample_name
-            print cmd
-            os.system(cmd)
-            cmd = "sed -e \'s/SESSION_NAME/%s/g\' temp1.json >  temp2.json" % ses["name"]
-            print cmd
-            os.system(cmd)
-            cmd = "sed -e \'s/FILE_PREFIX/%s/g\' temp2.json >  temp1.json" % filePrefix
-            print cmd
-            os.system(cmd)
-            cmd = "sed -e \'s/IMG_SIZE/%s/g\' temp1.json >  temp2.json" % img_size
-            print cmd
-            os.system(cmd)
-            if flg_mask > 0:
-                cmd = "sed -e \'s/MASK_IMG_NAME/%s/g\' temp2.json >  temp1.json" % sample_name
-                print cmd
-                os.system(cmd)
-            else:
-                cmd = "sed -e \'s/MASK_IMG_NAME/%s/g\' temp2.json >  temp1.json" % "white"
-                print cmd
-                os.system(cmd)
-            cmd = "cp temp1.json %s_%s_%s_%s.json" % ( engine, camera_name,  sample_name, ses["name"] )
-            print cmd
-            os.system(cmd)
-            os.system('rm -f temp*.json')
-
+        with open('%s_%s_%s_%s.json' % (engine, camera_info["name"],  sample_info["name"], ses["name"] ) , 'w') as outf:
+            outf.write(fileStr)
